2f68af07bb27bef7ed37149d6eeb4726820f36e9
Ethan <ethan.cychen@gmail.com>
2021-07-21 01:48:52 +0800

    update etl_fake_data.py: change group names. create 200 users

--- etl_fake_data.py
+++ etl_fake_data.py
@@ -113,7 +113,7 @@ for i in range(len(review_difficulty_name)):
 
 #ReviewScore
 review_score_name = ["viewonly", "direct supervision", "indirect supervision", "distance/no supervision", "supervise others"]
-review_score_desc = ["只能觀察，不能操作", "須在教師直接指導下執行(direct supervision)", "需協助時立即找得到教師指導(indirect supervision)", "可獨立執行，僅需事後督導(distance/no supervision)", "可指導別人(supervise others)"]
+review_score_desc = ["Level 1  只能觀察，不能操作(老師在場)","Level 2  需在老師直接指導下執行(老師在場)", "Level 3  需協助時立即找得到老師指導(老師在附近)","Level 4  可獨立執行，僅需事後督導","Level 5  可指導別人"]
 for i in range(len(review_score_name)):
     db.session.add(ReviewScore(value=i+1, name=review_score_name[i], desc=review_score_desc[i]))
 
@@ -121,87 +121,107 @@ db.session.commit()
 
 
 #Group
-group_name = ["第一間醫院", "第二間醫院"]
-group_desc = ["第一間醫院的描述", "第二間醫院的描述"]
-for name, desc in zip(group_name, group_desc):
-    db.session.add(Group(name=name, desc=desc))
+group_names = [
+    "基隆長庚紀念醫院(基長)",
+"台大醫院(台大)",
+"三軍總醫院(三總)",
+"臺北榮民總醫院(北榮)",
+"馬偕紀念醫院(馬偕)",
+"國泰綜合醫院(國泰)",
+"臺北市立聯合醫院(北市聯)",
+"萬芳醫院(萬芳)",
+"新光吳火獅紀念醫院(新光)",
+"臺北醫學大學附設醫院(北醫)",
+"振興醫院(振興)",
+"亞東紀念醫院(亞東)",
+"耕莘醫院(耕莘)",
+"台北慈濟醫院(北慈)",
+"雙和醫院(雙和)",
+"林口長庚紀念醫院(林長)",
+"臺中榮民總醫院(中榮)",
+"中國醫藥大學附設醫院(中國醫)",
+"中山醫學大學附設醫院(中山醫)",
+"光田綜合醫院(光田)",
+"童綜合醫院(童綜合)",
+"台中慈濟醫院(中慈)",
+"彰化基督教醫院(彰基)",
+"台大醫院雲林分院(雲林台大)",
+"嘉義長庚紀念醫院(嘉長)",
+"成大醫院(成大)",
+"奇美醫院(奇美)",
+"國軍高雄總醫院(國高總)",
+"高雄醫學大學附設醫院(高醫)",
+"高雄榮民總醫院(高榮)",
+"高雄長庚紀念醫院(高長)",
+"義大醫院(義大)",
+"羅東博愛醫院 (羅東)",
+"花蓮慈濟醫院 (花慈)",
+]
+
+for name in group_names:
+    db.session.add(Group(name=name, desc=name+"_desc"))
 db.session.commit()
 
 # User
 # admin
-user = User(username="admin", email="epataiwan.official@gmail.com")
-user.set_password("admin")
-user.role = Role.query.filter(Role.name=="醫院管理者").first()
-user.internal_group = Group.query.filter(Group.name=="第一間醫院").first()
-user.external_groups.append(Group.query.filter(Group.name=="第二間醫院").first())
-db.session.add(user)
-
-#group1
-user = User(username="G1R1", email="G1R1@epa.com")
-user.set_password("G1R1")
-user.role = Role.query.filter(Role.name=="住院醫師-R1").first()
-user.internal_group = Group.query.filter(Group.name == "第一間醫院").first()
-user.external_groups.append(Group.query.filter(Group.name == "第二間醫院").first())
-db.session.add(user)
-
-user = User(username="G1R5",email="G1R5@epa.com")
-user.set_password("G1R5")
-user.role = Role.query.filter(Role.name=="住院醫師-R5(總醫師)").first()
-user.internal_group = Group.query.filter(Group.name == "第一間醫院").first()
-db.session.add(user)
-
-user = User(username="G1R6", email="G1R6@epa.com")
-user.set_password("G1R6")
-user.role = Role.query.filter(Role.name=="主治醫師").first()
-user.internal_group = Group.query.filter(Group.name == "第一間醫院").first()
-db.session.add(user)
-
-# group2
-user = User(username="G2R1", email="G2R1@epa.com")
-user.set_password("G2R1")
-user.role = Role.query.filter(Role.name=="住院醫師-R1").first()
-user.internal_group = Group.query.filter(Group.name == "第二間醫院").first()
-db.session.add(user)
-
-user = User(username="G2R5",email="G2R5@epa.com")
-user.set_password("G2R5")
-user.role = Role.query.filter(Role.name=="住院醫師-R5(總醫師)").first()
-user.internal_group = Group.query.filter(Group.name == "第二間醫院").first()
-db.session.add(user)
-
-user = User(username="G2R6", email="G2R6@epa.com")
-user.set_password("G2R6")
-user.role = Role.query.filter(Role.name=="主治醫師").first()
-user.internal_group = Group.query.filter(Group.name == "第二間醫院").first()
-user.external_groups.append(Group.query.filter(Group.name == "第二間醫院").first())
-db.session.add(user)
+all_groups = Group.query.all()
+all_roles = Role.query.filter(Role.is_manager == False).all()
+for i in range(200):
+    user = User()
+    
+    user.role = all_roles[int(random.random()*len(all_roles))]
+    
+    fake_groups = []
+    for j in range(int(random.random()*3)+1):
+        fake_groups.append(all_groups[int(random.random()*len(all_groups))])
+    fake_groups = list(set(fake_groups))
+    user.internal_group = fake_groups[0]
+    for j in range(1, len(fake_groups)):
+        user.external_groups.append(fake_groups[j])
+    user.username = fake_groups[0].name+"_"+user.role.name+"_"+str(i)
+    user.set_password(user.username)    
+    db.session.add(user)
 
 db.session.commit()
 
 # # use "append" to resolve bridging table
-
+all_users = User.query.all()
 # notifications
-for i in range(3):
+for i in range(10):
     notification = Notification(subject=f"test note {i}",msg_body=f'test note body {i}')
-    notification.user = User.query.filter(User.username == "admin").first()
+    notification.user = all_users[int(random.random()*len(all_users))]
     db.session.add(notification)
 db.session.commit()
 
 # ask review
-
-all_reviewers = User.query.join(Role).filter(Role.can_be_reviewer == True, Role.is_manager == False).all()
-all_reviewees = User.query.join(Role).filter(Role.can_be_reviewee == True, Role.is_manager == False).all()
+all_users = User.query.all()
 
 all_locations = Location.query.all()
 all_epa = EPA.query.all()
+review_source_req = ReviewSource.query.filter(ReviewSource.name=='request').first()
+review_source_new = ReviewSource.query.filter(ReviewSource.name=='new').first()
 for i in range(20):
+    
+    user = all_users[int(random.random()*len(all_users))]
     review = Review()
-    reviewee_options = [user for user in all_reviewees if user != review.reviewer ]
-    review.creator = reviewee_options[int(random.random()*len(reviewee_options))]
-    review.implement_date = datetime.date.fromisoformat('2019-12-04') + datetime.timedelta(days=int(random.random()*(1-random.random())*1000))
-    review.review_source = ReviewSource.query.filter(ReviewSource.name=="request").first()
-    review.reviewer = all_reviewers[int(random.random()*len(all_reviewers))]
-    review.reviewee = review.creator
+    review.creator = user
+    
+    if user.can_create_review():
+        review.reviewer = user
+        reviewee_options = user.get_potential_reviewees()
+        if len(reviewee_options) == 0:
+            continue
+        review.reviewee = reviewee_options[int(random.random()*len(reviewee_options))]
+        review.review_source = review_source_new
+        review.is_draft = True
+    else:
+        review.reviewee = user
+        reviewer_options = user.get_potential_reviewers()
+        if len(reviewer_options) == 0:
+            continue
+        review.reviewer = reviewer_options[int(random.random()*len(reviewer_options))]
+        review.review_source = review_source_req
+    
     review.location = all_locations[int(random.random()*len(all_locations))]
     review.epa = all_epa[int(random.random()*len(all_epa))]
     db.session.add(review)
